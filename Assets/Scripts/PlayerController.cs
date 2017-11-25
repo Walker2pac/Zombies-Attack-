341a259a3e149e6b07035428c23091c20bab013d
Walker-PC <ermekus@inbox.ru>
2017-11-26 00:46:15 +0600

    first commit.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rigidbody;
+    Animator animator;
+    public float speed;
+
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+	}
+	
+	void FixedUpdate () {
+
+            float vAxis = Input.GetAxis("Vertical") * speed;
+            vAxis *= Time.deltaTime;
+            Vector3 movement = new Vector3(0, vAxis, 0);
+            rigidbody.MovePosition(transform.position + movement);
+
+            if(vAxis != 0)
+            {
+                animator.SetInteger("State", 1);
+            }
+            else
+            {
+                animator.SetInteger("State", 0);
+            }
+      
+	}
+}
